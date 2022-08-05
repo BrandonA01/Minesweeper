9b32a2203c731a2f889f20b6ebfc60fc14bb608f
Brandon Allen <81233247+BrandonA01@users.noreply.github.com>
2022-08-05 09:09:52 +0100

    Commit

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,2 +1,14 @@
 public class Game {
+    public static final int  WIDTH = 700, HEIGHT = 700;
+    public static final int GRID = 20;
+    public static final int BOMBS = 50;
+
+    public Game(){
+
+    }
+
+    public static void main(String[] args){
+
+    }
+
 }
